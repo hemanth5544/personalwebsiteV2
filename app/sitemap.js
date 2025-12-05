de8ee7fc21a4823ad3486017574df7245d17491d
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2025-12-05 23:25:50 +0530

    changes

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://hemanthrachapalli.up.railway.app/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
