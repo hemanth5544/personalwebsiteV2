cd06df946d711f769b5c6532e1e479119761deeb
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2025-12-07 11:17:15 +0530

    assests

---
+++ app/data.js
@@ -0,0 +1,50 @@
+
+
+export const skillsData = {
+  languages: [
+    { name: "JavaScript", iconUrl: "https://skillicons.dev/icons?i=js", color: "#F7DF1E" },
+    { name: "TypeScript", iconUrl: "https://skillicons.dev/icons?i=ts", color: "#3178C6" },
+    { name: "Go", iconUrl: "https://skillicons.dev/icons?i=go", color: "#00ADD8" },
+    { name: "Java", iconUrl: "https://skillicons.dev/icons?i=java", color: "#ED8B00" },
+  ],
+  backend: [
+    { name: "Node.js", iconUrl: "https://skillicons.dev/icons?i=nodejs", color: "#339933" },
+    { name: "Express.js", iconUrl: "https://skillicons.dev/icons?i=express", color: "#68CC00" },
+    { name: "MongoDB", iconUrl: "https://skillicons.dev/icons?i=mongodb", color: "#47A248" },
+    { name: "MySQL", iconUrl: "https://skillicons.dev/icons?i=mysql", color: "#4479A1" },
+    { name: "PostgreSQL", iconUrl: "https://skillicons.dev/icons?i=postgresql", color: "#336791" },
+    { name: "Socket", iconUrl: "https://socket.io/images/favicon.png", color: "#f0e871ff" },
+    { name: "Sequelize", iconUrl: "https://skillicons.dev/icons?i=sequelize", color: "#0C344B" },
+    { name: "NestJs", iconUrl: "https://skillicons.dev/icons?i=nestjs	", color: "#0C344B" },
+    { name: "GraphQL", iconUrl: "https://skillicons.dev/icons?i=graphql", color: "#E10098" },
+    { name: "Supabase", iconUrl: "https://skillicons.dev/icons?i=supabase", color: "#3ECF8E" },
+
+  ],
+  tools: [
+    { name: "Linux", iconUrl: "https://skillicons.dev/icons?i=linux", color: "#FCC624" },
+    { name: "Bitbucket", iconUrl: "https://skillicons.dev/icons?i=bitbucket", color: "#205081" },
+    { name: "Git", iconUrl: "https://skillicons.dev/icons?i=git", color: "#F05032" },
+    { name: "VS Code", iconUrl: "https://skillicons.dev/icons?i=vscode", color: "#007ACC" },
+    { name: "Docker", iconUrl: "https://skillicons.dev/icons?i=docker", color: "#2496ED" },
+    { name: "AWS", iconUrl: "https://skillicons.dev/icons?i=aws", color: "#FF9900" },
+    { name: "Vercel", iconUrl: "https://skillicons.dev/icons?i=vercel", color: "#000000" },
+    {name: "Railway", iconUrl: "https://railway.app/favicon.ico", color: "#000000" },
+    
+  ],
+  libraries: [
+    { name: "Redis", iconUrl: "https://skillicons.dev/icons?i=redis", color: "#5C3EE8" },
+    { name: "Kafka", iconUrl: "https://skillicons.dev/icons?i=kafka", color: "#F7931E" },
+    { name: "Deno", iconUrl: "https://skillicons.dev/icons?i=deno", color: "#FFCC4D" },
+    { name: "Jest", iconUrl: "https://skillicons.dev/icons?i=jest", color: "#C21325" },
+    { name: "Elasticsearch", iconUrl: "https://skillicons.dev/icons?i=elasticsearch", color: "#FF6F00" },
+    { name: "Bun", iconUrl: "https://skillicons.dev/icons?i=bun", color: "#EE4C2C" },
+    { name: "Electron", iconUrl: "https://skillicons.dev/icons?i=electron", color: "#47848F" }, 
+  ],
+  hardware: [
+    { name: "Nginx", iconUrl: "https://skillicons.dev/icons?i=nginx", color: "#009639" },
+    { name: "N8N", iconUrl: "https://n8n.io/favicon.ico", color: "#00979D" },
+    { name: "New Relic", iconUrl: "https://newrelic.com/themes/custom/erno/assets/images/metadata/favicon.ico", color: "#A22846" },
+    { name: "Prometheus", iconUrl: "https://skillicons.dev/icons?i=prometheus", color: "#00979D" },
+    { name: "Turbo Repo", iconUrl: "https://turborepo.com/images/product-icons/repo-light-32x32.png", color: "#F46800" },
+  ]
+}
