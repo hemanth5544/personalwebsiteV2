ef74dd2328c2afefa16826a90c27e406a44c2b1b
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2026-04-20 15:31:19 +0530

    removed resend keys

--- app/api/send-voice/route.ts
+++ app/api/send-voice/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import dotenv from 'dotenv'
 dotenv.config();
-const resend = new Resend(process.env.RESEND_KEY);
+const resend = new Resend("notneeded");
 
 export async function POST(request: NextRequest) {
   try {
